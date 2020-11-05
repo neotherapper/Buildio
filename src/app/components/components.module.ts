6b34bc2eabc68847efdd452b7c0ab96dd72fa1c3
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-11-05 18:02:09 +0000

    added components module to use across the app

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export const COMPONENTS = [
+];
+
+
+@NgModule({
+  declarations: [COMPONENTS],
+  imports: [CommonModule],
+  exports: COMPONENTS,
+})
+export class ComponentsModule {}
