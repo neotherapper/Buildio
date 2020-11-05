784f59a61b966156a62d569c3962a35cf0ffa740
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-11-05 21:53:31 +0000

    added input for adding nicknames and vertically nickname list at nickname editor

--- src/app/components/nickame-editor/nickame-editor.component.ts
+++ src/app/components/nickame-editor/nickame-editor.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-nickame-editor',
   templateUrl: './nickame-editor.component.html',
   styleUrls: ['./nickame-editor.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NickameEditorComponent implements OnInit {
+export class NickameEditorComponent {
+  selectable = true;
+  removable = true; // this can be an Input on future refactor
+  addOnBlur = true;
 
-  constructor() { }
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
 
-  ngOnInit(): void {
-  }
+  @Input() nicknames!: string[];
 
+  /**
+   * Presentational components receive data through @Input() and communicate events
+   * through @Output() but generally maintain no internal state of their
+   * own. All decisions are delegated to 'container', or 'smart'
+   * components before data updates flow back down.
+   *
+   * More on 'smart' and 'presentational' components: https://gist.github.com/btroncone/a6e4347326749f938510#utilizing-container-components
+   */
+
+  @Output() add = new EventEmitter<string>();
+  @Output() remove = new EventEmitter<string>();
 }
