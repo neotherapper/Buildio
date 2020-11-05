2c81467d0625206ea0c16e88b3714aba29c43467
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-11-05 21:49:19 +0000

    added material MatChipsModule to use for the nickname editor

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,35 +3,31 @@ import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDialogModule } from '@angular/material/dialog';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatChipsModule } from '@angular/material/chips';
+
+
 
 @NgModule({
   imports: [
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatSidenavModule,
     MatListModule,
     MatIconModule,
-    MatToolbarModule,
-    MatProgressSpinnerModule,
-    MatDialogModule
+    MatDialogModule,
+    MatChipsModule,
   ],
   exports: [
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatSidenavModule,
     MatListModule,
     MatIconModule,
-    MatToolbarModule,
-    MatProgressSpinnerModule,
-    MatDialogModule
-  ]
+    MatDialogModule,
+    MatChipsModule,
+  ],
 })
 export class MaterialModule {}
