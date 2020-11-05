6541ae7afa8c723191f5f72caaace5313d2237e8
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-11-05 13:36:29 +0000

    added title to the building detail ui component

--- src/app/building/components/building-detail/building-detail.component.spec.ts
+++ src/app/building/components/building-detail/building-detail.component.spec.ts
@@ -1,10 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
+import { Building } from '@buildio/building/models';
 import { BuildingDetailComponent } from './building-detail.component';
 
 describe('BuildingDetailComponent', () => {
   let component: BuildingDetailComponent;
   let fixture: ComponentFixture<BuildingDetailComponent>;
+  let buildingDe;
+  let buildingEl;
+  let expectedBuilding: Building;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +21,36 @@ describe('BuildingDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BuildingDetailComponent);
     component = fixture.componentInstance;
+
+    // find the building's DebugElement and element
+    buildingDe = fixture.debugElement.query(By.css('.building-detail'));
+    buildingEl = buildingDe.nativeElement;
+
+    // mock the building supplied by the parent component
+    expectedBuilding = {
+      id: 17,
+      nicknames: [],
+      address: {
+        city: 'London',
+        street: '30 St Mary Axe',
+      },
+      description: 'Test description',
+    };
+
+    // simulate the parent setting the input property with that building
+    component.building = expectedBuilding;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display Title in uppercase', () => {
+    const expectedTitle = `${expectedBuilding.address.street}, ${expectedBuilding.address.city}`.toUpperCase();
+    expect(buildingEl.textContent).toContain(expectedTitle);
+  });
+
+
 });
--- src/app/building/components/building-detail/building-detail.component.ts
+++ src/app/building/components/building-detail/building-detail.component.ts
@@ -1,16 +1,44 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Address, Building, Room } from '@buildio/building/models';
 
 @Component({
   selector: 'app-building-detail',
   templateUrl: './building-detail.component.html',
   styleUrls: ['./building-detail.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BuildingDetailComponent implements OnInit {
+export class BuildingDetailComponent {
+  /**
+   * Presentational components receive data through @Input() and communicate events
+   * through @Output() but generally maintain no internal state of their
+   * own. All decisions are delegated to 'container', or 'smart'
+   * components before data updates flow back down.
+   *
+   * More on 'smart' and 'presentational' components: https://gist.github.com/btroncone/a6e4347326749f938510#utilizing-container-components
+   */
 
-  constructor() { }
+  @Input() building!: Building;
 
-  ngOnInit(): void {
+  /**
+   * Tip: Utilize getters to keep templates clean
+   */
+  get id(): number {
+    return this.building.id;
   }
 
+  get address(): Address {
+    return this.building.address;
+  }
+
+  get description(): string {
+    return this.building.description;
+  }
+
+  get nicknames(): string[] {
+    return this.building.nicknames;
+  }
+
+  get rooms(): Room[] {
+    return this.building.rooms;
+  }
 }
